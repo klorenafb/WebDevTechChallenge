86b2ea8a63eb1a5fd381cba9dbfbf7877bba05a4
Rena <EQUIPOS DE BOMBEO@Rena.Rena>
2017-09-24 19:24:51 -0500

    Fisrt commit

--- Step2/chat.js
+++ Step2/chat.js
@@ -1,6 +1,6 @@
 <script type="text/javascript">
   var url = window.location.href;
-  var locale = url.split("/")[5];
+  var locale = url.split("/")[3];
   var onlineGreeting, offlineGreeting;
 
   if(locale === "es") {
@@ -53,38 +53,6 @@
       });
     });
   } 
-   else if(locale === "hi") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "हमारे साथ चैट करें";
-    offlineGreeting = "हमें एक संदेश छोड़ दो";
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
-  }
-   else if(locale === "pl") {
-    window.$zopim||(function(d,s){var z=$zopim=function(c){
-    z._.push(c)},$=z.s=
-    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?';z.t=+new Date;$.
-    type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-    onlineGreeting = "Rozmawiaj z nami";
-    offlineGreeting = "Zostaw nam wiadomość";
-    $zopim(function() {
-      $zopim.livechat.setGreetings({
-      'online': onlineGreeting,
-      'offline': offlineGreeting
-      });
-    });
-  }
   else {
     window.$zopim||(function(d,s){var z=$zopim=function(c){
     z._.push(c)},$=z.s=
